dded94fd950bdf027f0b13745a902d115add6e77
tfchan <cseelemon@gmail.com>
2019-03-08 17:23:16 +0800

    Add function to read data from input file

--- hw1.py
+++ hw1.py
@@ -2,6 +2,13 @@
 from argparse import ArgumentParser
 
 
+def read_input(file_name):
+    """Read and return all lines from input file."""
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+
 def main():
     """Parse arguments and pass them to task."""
     parser = ArgumentParser(description='Linear regression of 1-D input')
@@ -9,6 +16,7 @@ def main():
     parser.add_argument('n', type=int, help='Number of polynomial bases')
     parser.add_argument('lambda', type=int, help='Lambda parameter for LSE')
     args = parser.parse_args()
+    lines = read_input(args.file_name)
 
 
 if __name__ == '__main__':
