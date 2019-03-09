67a43ae7837e11d8fa9cf678dd4a30db998eb1b7
tfchan <cseelemon@gmail.com>
2019-03-09 17:38:57 +0800

    Rename variable to a shorter one

--- matrix.py
+++ matrix.py
@@ -2,23 +2,23 @@
 import numpy as np
 
 
-def _pivoting(matrix_a):
-    """Pivoting matrix_a."""
-    matrix_size = matrix_a.shape[0]
-    id_matrix = np.identity(matrix_size)
-    matrix_pivoted = np.copy(matrix_a)
-    for j in range(matrix_size):
+def _pivoting(m):
+    """Pivoting matrix m."""
+    m_size = m.shape[0]
+    identity = np.identity(m_size)
+    m_copy = np.copy(m)
+    for j in range(m_size):
         # Find row with absolutely largest element at column j
-        row = max(range(j, matrix_size), key=lambda i: abs(matrix_a[i][j]))
+        row = max(range(j, m_size), key=lambda i: abs(m_copy[i][j]))
         if j != row:
-            id_matrix[[j, row]] = id_matrix[[row, j]]
-            matrix_pivoted[[j, row]] = matrix_pivoted[[row, j]]
-    return id_matrix, matrix_pivoted
+            identity[[j, row]] = identity[[row, j]]
+            m_copy[[j, row]] = m_copy[[row, j]]
+    return identity, m_copy
 
 
-def lu_decomposition(matrix_a):
-    """Do LU decomposition on the square matrix matrix_a."""
-    assert matrix_a.ndim == 2, 'Input matrix must be a 2d-array'
-    assert matrix_a.shape[0] == matrix_a.shape[1], 'Must input square matrix'
+def lu_decomposition(a):
+    """Do LU decomposition on the square matrix matrix a."""
+    assert a.ndim == 2, 'Input matrix must be a 2d-array'
+    assert a.shape[0] == a.shape[1], 'Must input square matrix'
 
-    matrix_p, matrix_pa = _pivoting(matrix_a)
+    p, pa = _pivoting(a)
