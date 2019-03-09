4b804324e0f7f7f41f966e1f54c0417109b09fd9
tfchan <cseelemon@gmail.com>
2019-03-10 02:40:21 +0800

    Complete matrix inverse function using lu

--- matrix.py
+++ matrix.py
@@ -34,3 +34,39 @@ def lu_decomposition(a):
             l_[i][j] = pa[i][j] - sum(l_[i][k] * u[k][j] for k in range(j))
             l_[i][j] /= u[j][j]
     return p, l_, u
+
+
+def inverse_of_l(l_):
+    """Find inverse of lower triangular matrix l_ with diagonal of 1."""
+    l_size = l_.shape[0]
+    l_inv = np.zeros((l_size, l_size))
+    for j in range(l_size):
+        l_inv[j][j] = 1
+        for i in range(j + 1, l_size):
+            l_inv[i][j] = -sum(l_[i][k] * l_inv[k][j] for k in range(i))
+    return l_inv
+
+
+def inverse_of_u(u):
+    """Find inverse of upper triangular matrix u with diagonal of 0."""
+    u_size = u.shape[0]
+    u_inv = np.zeros((u_size, u_size))
+    for i_ in range(u_size):
+        i = u_size - i_ - 1
+        for j in range(i, u_size):
+            u_inv[i][j] = -sum(
+                u[i][k] * u_inv[k][j] for k in range(i + 1, j + 1))
+            u_inv[i][j] = 1 / u[i][i] if i == j else u_inv[i][j] / u[i][i]
+    return u_inv
+
+
+def inverse(a):
+    """Find inverse of square matrix a."""
+    assert a.ndim == 2, 'Input matrix must be a 2d-array'
+    assert a.shape[0] == a.shape[1], 'Must input square matrix'
+
+    p, l_, u = lu_decomposition(a)
+    l_inv = inverse_of_l(l_)
+    u_inv = inverse_of_u(u)
+    a_inv = np.dot(u_inv, l_inv)
+    return a_inv
